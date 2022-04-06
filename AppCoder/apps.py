a4bd82f9b3f1d3414ecd04820954702cf7cf585d
Meli Casola <melissa.casola@mi.unc.edu.ar>
2022-04-06 01:15:08 -0300

    hasta aca hice pagina web

--- AppCoder/apps.py
+++ AppCoder/apps.py
@@ -1,6 +1,15 @@
-from django.apps import AppConfig
+# from django.apps import AppConfig
+# from django.contrib import admin
+# from .models import *
 
+# class AppcoderConfig(AppConfig):
+#     default_auto_field = 'django.db.models.BigAutoField'
+#     name = 'AppCoder'
 
-class AppcoderConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'AppCoder'
+# admin.site.register(Curso)
+
+# admin.site.register(Estudiante)
+
+# admin.site.register(Profesor)
+
+# admin.site.register(Entrega)
--- AppCoder/views.py
+++ AppCoder/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 from django.http import HttpResponse
 #from models import Curso
 from AppCoder.models import Curso
@@ -12,3 +12,34 @@ def curso(self):
     documento = f"El Curso es de: {curso.nombre}, la camada es: {curso.camada}"
     
     return HttpResponse(documento)
+
+# def inicio(request):
+#     return  HttpResponse('vista inicio')
+
+# def curso(request):
+#     return HttpResponse('vista curso')
+
+# def profesores(request):
+#     return HttpResponse('vista profesores')
+
+# def estudiantes(request):
+#     return HttpResponse('vista estudiantes')
+
+# def entregables(request):
+#     return HttpResponse('vista entregables')
+
+    
+def inicio(request):
+    return  render(request, "AppCoder/inicio.html")
+
+def cursos(request):
+    return  render(request, "AppCoder/cursos.html")
+
+def profesores(request):
+    return  render(request, "AppCoder/profesores.html")
+
+def estudiantes(request):
+    return  render(request, "AppCoder/estudiantes.html")
+
+def entregables(request):
+    return  render(request, "AppCoder/entregables.html")
